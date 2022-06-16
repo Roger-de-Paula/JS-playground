429f3a339fbd52aef5d868e91128779ab4fe6ce9
Roger_Tech <rogerio.gss123@gmail.com>
2022-06-16 19:22:21 -0300

    project2 ready

---
+++ project2/app.js
@@ -0,0 +1,44 @@
+let data = [
+    {
+        name:'Roger',
+        age:'50'
+    },
+    {
+        name:'Lila',
+        age:'26'
+    },
+    {
+        name:'Bob',
+        age:'20'
+    },
+    {
+        name:'Ashley',
+        age:'25'
+    },
+    {
+        name:'Roger',
+        age:'50'
+    },
+    {
+        name:'Lila',
+        age:'26'
+    },
+    {
+        name:'Bob',
+        age:'20'
+    },
+    {
+        name:'Ashley',
+        age:'25'
+    },
+
+
+];
+
+const info = document.querySelector('#info');
+
+let details = data.map(function(item) {
+    return `<div> ${item.name} is ${item.age} years old </div>`;
+});
+
+info.innerHTML = details.join('\n');
