2c85a769b78ac8d4c40f798fe2e85844a122b6c4
Roger_Tech <rogerio.gss123@gmail.com>
2022-06-16 17:21:28 -0300

    project1 working

--- project1/app.js
+++ project1/app.js
@@ -1,13 +1,17 @@
 const closedFace = document.querySelector('.closed');
 const openFace = document.querySelector('.open');
 
-console.log(openFace)
-console.log(closedFace)
-
 //event listener
 closedFace.addEventListener('click', () => {
     if (openFace.classList.contains('open')) {
-        console.log('hello');
         openFace.classList.add('active');
+        closedFace.classList.remove('active');
+    }
+});
+
+openFace.addEventListener('click', () => {
+    if (openFace.classList.contains('active')) {
+        closedFace.classList.add('active');
+        openFace.classList.remove('active');
     }
 });
